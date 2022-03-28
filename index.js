b984cb3e125cde0d3a2141e007abb4866c110544
Александ Мясоед @alexproit Telegram <79302838+myasoedas@users.noreply.github.com>
2022-03-28 19:31:25 +0300

    feat: отредактировал файл index.js
    отредактировал файл index.js стандартного шаблона проекта react

---
+++ index.js
@@ -0,0 +1,53 @@
+// Рассмотим стандартный шаблон проекта React
+// Из примера 002 вставим код, который внутри тега <script> в файл index.js
+// при этом отменим импорт import './index.css'; и import App from './App';
+// Запустим наш код через терминал командой npm start - на экране браузера увидим результат из примера 002
+// В данном случае мы не использовали CDN а настроили предварительно окружение - установили NodeJS и 
+// создали стандартный шаблон React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css'; - не будем импортировать index.css который предоставляет шаблон проекта по умолчанию
+//import App from './App'; - не будем импортировать App который предоставляет шаблон проекта по умолчанию
+import reportWebVitals from './reportWebVitals';
+
+
+// Создадим компонент Book со свойствами название, год, цена, которые будем хранить в объекте props
+const Book = (props) => {
+// Создадим блок div, с карточкой книги, содержимое представим в виде массива встроеных компонентов: название, год, цена, которые будем хранить в объекте props
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, props.name),
+        React.createElement("p", {}, props.year),
+        React.createElement("p", {}, props.price),
+        // Сделал компонент заготовку, в котоорую при отрисовке буду подставлять нужные данные
+      ])
+    }
+// В компонент App добавлю - компонент Book столько раз сколько мне потребуется и каждый раз буду задавать новые данные
+const App = () => {
+    return React.createElement("div", {}, [
+        // Компонен заголовок h1
+        React.createElement("h1", {id: 'hello', classname: 'class1'}, 'Книжный магазин'),
+        // Компонент Book с подстановкой актуальных данных
+        React.createElement(Book, {name: 'HTML5', year: 2020,  price: 450}),
+        // Компонент Book с подстановкой актуальных данных
+        React.createElement(Book, {name: 'CSS3', year: 2021,  price: 450}),
+        // Компонент Book с подстановкой актуальных данных
+        React.createElement(Book, {name: 'JS', year: 2022,  price: 450}),
+        // Компонент Book с подстановкой актуальных данных
+        React.createElement(Book, {name: 'React', year: 2022,  price: 650}),
+      ])
+  }
+// Отрисовываем приложение App с компонентами React: h1, Book которые мы создали внутри него
+// отрисовку проводим в блоке div с id 'root'
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
